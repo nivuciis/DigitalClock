acdabc82e3ab60eb087248466153c8a115eee7eb
nivuciis <vinicius.rafael751@gmail.com>
2025-04-13 19:18:22 -0300

    feat(FirstState): TimerInterrupt with clock

--- DigitalClock/DigitalClock/main.asm
+++ DigitalClock/DigitalClock/main.asm
@@ -7,30 +7,29 @@
 
 
 ;Definições de clock
-#define CLOCK 16.0e6 ;clock speed
-#define DELAY 1 ;seconds
-.equ PRESCALE = 0b100 ;/256 prescale
-.equ PRESCALE_DIV = 256
-.equ WGM = 0b0100
-.equ TOP = int(0.5 + ((CLOCK/PRESCALE_DIV)*DELAY))
-.if TOP > 65535
-.error "TOP is out of range"
-.endif
-
+.equ DISPLAY_DELAY = 5 ; ms
+.equ TIMER_PRESCALE = 64 
+.equ TIMER_COMPARE = (16000000/64/1000)*DISPLAY_DELAY ; Valor para comparar
 .dseg 
+.org SRAM_START
 current_digit: .byte 1
-HOUR: .byte 2
-MINUTES: .byte 2
+hora_dezena: .byte 1
+hora_unidade: .byte 1
+minuto_dezena: .byte 1
+minuto_unidade: .byte 1
+display_atual: .byte 1
+contador_timer: .byte 2
+
 .cseg
 .org 0x0000
 		rjmp reset
-.org 0x0016        ; Timer1 Compare A interrupt vector
-    rjmp TIMER1_COMPA_ISR ; Timer dos segundos
+.org OVF1addr
+	rjmp TIMER1_OVF_ISR
 
 STATE_TABLE: ;Armazena o local em que os estados estão alocados
-    .dw state0_operation ,state1_operation ,CRON_RUN
-	.dw state3_operation ,state4_operation ,state5_operation 
-	.dw state6_operation  ,state7_operation 
+    .dw  TEMPO_INC, PRE_TWO ,CRON_RUN
+	.dw CRON_STOP , PRE_THREE ,BLINK
+	.dw MOV_SELECT  ,SELECT_INC
 
 DIGITS_TABLE: ;Armazena os caracteres 0-9 em forma binaria
 	.db 0b00111111, 0b00000110  ; 0, 1
@@ -66,11 +65,25 @@ reset:
 	ldi stack_reg, high(RAMEND)
 	out SPH, stack_reg
 	;Definindo o estado atual para o primeiro estado na tabela
-	ldi current_state, 0
+	ldi current_state, state0
 	ser temp
-	out DDRC, temp ; seta a porta C como saida
-	out DDRB, temp ; porta B também pra jogar nos mosfets
-	clr temp 
+	out DDRD, temp ; seta a porta C como saida dos displays
+	ldi temp, 0x0F ;seta os 4 primeiros pinos da porta B como saida de controle 
+	out DDRB, temp
+	
+	;Inicialização das variaveis
+	ldi temp, 1
+	sts hora_dezena, temp
+	ldi temp, 3
+	sts hora_unidade, temp
+	sts minuto_dezena, temp
+	clr temp
+	sts minuto_unidade, temp
+	sts display_atual, temp
+	sts contador_timer, temp
+	sts contador_timer+1, temp
+
+	; Configurar timer 1 para interrupção por overflow 
 
 	sei ; Habilitar interrupções
 	call transition ;Pula direto para o estado de transição
@@ -79,34 +92,144 @@ main:
 	rjmp main
 
 setup_timer:
-    ldi r16, (1 << WGM12) | (1 << CS12)  ; CTC mode, prescaler=256
+    ldi temp, high(TIMER_COMPARE)
+	sts OCR1AH, temp
+	ldi temp, low(TIMER_COMPARE)
+	sts OCR1AL, temp
 
-    sts TCCR1B, r16
-    ldi r16, high(TOP)                  ; Delay de 1s @ 16MHz
-    sts OCR1AH, r16
-    ldi r16, low(TOP)
-    sts OCR1AL, r16	
-    ldi r16, (1 << OCIE1A)               ; Habilita a interrupção do timer 1
-    sts TIMSK1, r16
-	ser mosfet
-	out PORTB, mosfet
-    ret
+	ldi temp, (1<<WGM12)| (1<<CS11)|(1<<CS10) ; AJuste do ctc para um prescale de 64
+	sts TCCR1B, temp
+	ldi temp, (1<<OCIE1A) ; interrupcao por comparacao
+	sts TIMSK1, temp
+	ret
 
 
 ; --- Timer1 interrupt handler ---
-TIMER1_COMPA_ISR:
-    push r16
-    lds r16, current_digit
-    inc r16
-    cpi r16, 10
-    brlo save_digit
-    clr r16                   ; Volta para o 0 depois de 9
-	save_digit:
-		sts current_digit, r16
-		pop r16
-		reti
+TIMER1_OVF_ISR:
+    push temp
+    push stack_reg
+	push mosfet
+	in temp, sreg
+	push temp
 
-irq:
+	lds temp, display_atual
+
+	lds stack_reg, 0x0F
+	out PORTB, stack_reg
+
+	cpi temp, 0
+	breq mostrar_hora_dezena
+	cpi temp, 1
+	breq mostrar_hora_unidade
+	cpi temp, 2
+	breq mostrar_minuto_dezena
+	cpi temp, 3
+	breq mostrar_minuto_unidade
+
+	mostrar_hora_dezena:
+		lds stack_reg, hora_dezena
+		rjmp show_digits
+	mostrar_hora_unidade:
+		lds stack_reg, hora_unidade
+		rjmp show_digits
+	mostrar_minuto_dezena:
+		lds stack_reg, minuto_dezena
+		rjmp show_digits
+	mostrar_minuto_unidade:
+		lds stack_reg, minuto_unidade
+		
+	show_digits:
+		ldi ZL, low(DIGITS_TABLE<<1)
+		ldi ZH, high(DIGITS_TABLE<<1)
+		add ZL, stack_reg
+		lpm temp, Z
+
+		out PORTD, temp
+
+		ldi stack_reg, 0x0F
+		com temp
+		and stack_reg, temp
+		out PORTB, stack_reg
+
+		lds temp, display_atual
+		inc temp
+		cpi temp, 4
+		brne save_actual_display
+		ldi temp, 0
+		save_actual_display:
+			sts display_atual, temp
+
+		lds temp, contador_timer
+		lds stack_reg, contador_timer+1
+		inc stack_reg
+		inc temp
+		cpi temp, low(200)
+		ldi mosfet, high(200)
+		cpc stack_reg, mosfet
+		brne end_timerISR
+		clr temp
+		clr stack_reg
+		clr mosfet
+		rcall atualiza_tempo
+		end_timerISR:
+			sts contador_timer, temp
+			sts contador_timer+1, stack_reg
+			pop temp
+			out sreg, temp
+			pop mosfet
+			pop stack_reg
+			pop temp
+			reti
+
+atualiza_tempo:
+	push temp
+	push stack_reg
+
+	lds temp, minuto_unidade
+	inc temp
+	cpi temp, 10
+	brne salva_minuto_unidade
+	clr temp
+	lds stack_reg, minuto_dezena
+	inc stack_reg
+	cpi stack_reg, 6
+	brne salva_minuto_dezena
+
+	clr stack_reg
+	lds temp, hora_unidade
+	inc temp
+	cpi temp, 10
+	brne salva_hora_unidade
+
+	clr temp
+	lds stack_reg, hora_dezena
+	inc stack_reg
+	cpi stack_reg, 3
+	brne salva_hora_dezena
+
+	clr temp
+	clr stack_reg
+	sts hora_dezena, temp
+	sts hora_unidade, temp
+	rjmp fim_atualizacao
+			
+	salva_hora_dezena:
+		sts hora_dezena, stack_reg
+		rjmp fim_atualizacao
+	salva_hora_unidade:
+		sts hora_unidade, temp
+		rjmp fim_atualizacao
+	salva_minuto_dezena:
+		sts minuto_dezena, stack_reg
+		rjmp salva_minuto_unidade
+	salva_minuto_unidade:
+		sts minuto_unidade, temp
+	fim_atualizacao:
+		pop stack_reg
+		pop temp
+		ret
+
+MODE_ISR:
 	push temp
 	in temp, sreg
 	push temp
@@ -148,7 +271,7 @@ irq:
 		reti
 
 ; interrupção para quando apertar "START"
-irq_start:
+START_ISR:
 	push temp
 	in temp, sreg
 	push temp
@@ -190,7 +313,7 @@ irq_start:
 		reti
 
 ; interrupção para quando apertar "RESET"
-irq_reset:
+RESET_ISR:
     push temp
     in temp, sreg
     push temp
@@ -258,19 +381,32 @@ transition:
 	movw ZL, R0
 	ijmp
 
-state0_operation:
+
+
+;---------------------MODO 1 RELOGIO-----------------------------
+TEMPO_INC:
     cpi current_state, state0
     brne transition ; se NÃO for o estado 0, pula pra transition
-	rjmp state0_operation
+	call setup_timer
+	call tempo_inc_loop
+	tempo_inc_loop:
+		nop
+		rjmp tempo_inc_loop
+	
+	rjmp TEMPO_INC
 
-state1_operation:
+
+;------------------------------MODO 2 CRONOMETRO ZERADO---------------------
+PRE_TWO:
     cpi current_state, state1
     brne transition 
 
 	; lógica
 
-    rjmp state1_operation
+    rjmp PRE_TWO
+	
 
+;------------------------------MODO 2 CRONOMETRO RODANDO--------------------------
 CRON_RUN:
     cpi current_state, state2
     brne transition 
@@ -288,15 +424,18 @@ CRON_RUN:
 
     rjmp CRON_RUN
 
-state3_operation:
+;---------------------------------------MODO 2 CRONOMETRO PARADO--------------------------
+CRON_STOP:
     cpi current_state, state3
     brne transition 
 
 	; lógica
 
-    rjmp state3_operation
+    rjmp CRON_STOP
 
-state4_operation:
+
+;------------------------- MODO 3 APENAS BIP------------------------------------
+PRE_THREE:
     ; desabilita interrupções
     cli
     
@@ -306,15 +445,18 @@ state4_operation:
     sei
     rjmp main
 
-state5_operation:
+;-----------------------------------MODO 3 DISPLAY SELECIONADO PISCANDO----------------------------
+BLINK:
     cpi current_state, state5
     brne transition 
 
 	; lógica
 
-    rjmp state5_operation
+    rjmp BLINK
 
-state6_operation:
+
+;------------------------------------------MODO 3 MUDA O DISPLAY QUE SERA SELECIONADO----------------------------
+MOV_SELECT:
     ; desabilita interrupções
     cli
     
@@ -324,7 +466,8 @@ state6_operation:
     sei
     rjmp main
 
-state7_operation:
+;----------------------------------------------- MODO 3 MUDA O VALOR NO DISPLAY SELECIONADO-------------------------------
+SELECT_INC:
     ; desabilita interrupções
     cli
     
@@ -334,6 +477,8 @@ state7_operation:
     sei
     rjmp main
 
+
+;----------------------------------- FUNC PARA INCREMENTAR TIMER --------------------------------------
 inc_timer:
 	push temp
 	in temp, sreg
